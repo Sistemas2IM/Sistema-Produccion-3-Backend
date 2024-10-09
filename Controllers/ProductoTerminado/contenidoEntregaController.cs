a77eaccfa1171b28331390ab9b573670c17a39bf
Sistemas2 <a.sistemas2@im.com.sv>
2024-10-09 14:25:02 -0600

    Se agregaron los endpoint's para las tablas detalle. Producto Terminado

---
+++ Controllers/ProductoTerminado/contenidoEntregaController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sistema_Produccion_3_Backend.DTO.ProductoTerminado;
+using Sistema_Produccion_3_Backend.Models;
+
+namespace Sistema_Produccion_3_Backend.Controllers.ProductoTerminado
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class contenidoEntregaController : ControllerBase
+    {
+        private readonly base_nuevaContext _context;
+        private readonly IMapper _mapper;
+
+        public contenidoEntregaController(base_nuevaContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        // GET: api/contenidoEntrega
+        [HttpGet("get")]
+        public async Task<ActionResult<IEnumerable<ContenidoEntregaDto>>> GetcontenidoEntrega(int idEntregaPt)
+        {
+            var contenidoEntrega = await _context.contenidoEntrega
+                .Where(d => d.idEntregaPt == idEntregaPt)
+                .ToArrayAsync();
+
+            if (contenidoEntrega == null || contenidoEntrega.Length == 0) 
+            {
+                return NotFound("No se encontraron contenidos relacionados con el reporte con id: " + idEntregaPt);
+            }
+
+            var contenidoEntregaDto = _mapper.Map<List<ContenidoEntregaDto>>(contenidoEntrega);
+
+            return Ok(contenidoEntregaDto);
+        }
+
+        // GET: api/contenidoEntrega/5
+        [HttpGet("get/id/")]
+        public async Task<ActionResult<ContenidoEntregaDto>> GetcontenidoEntregaId(int id)
+        {
+            var contenidoEntrega = await _context.contenidoEntrega.FindAsync(id);
+
+            var contenidoEntregaDto = _mapper.Map<ContenidoEntregaDto>(contenidoEntrega);
+
+            if (contenidoEntregaDto == null)
+            {
+                return NotFound("No se encuentra el contenido con el id: " + id);
+            }
+
+            return Ok(contenidoEntregaDto);
+        }
+
+        // PUT: api/contenidoEntrega/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("put/id/")]
+        public async Task<IActionResult> PutcontenidoEntrega(int id, UpdateContenidoEntregaDto updateContenidoEntrega)
+        {
+            var contenidoEntrega = await _context.contenidoEntrega.FindAsync(id);
+
+            if (contenidoEntrega == null)
+            {
+                return NotFound("No se encontro el contenido con el id: " + id);
+            }
+
+            _mapper.Map(updateContenidoEntrega, contenidoEntrega);
+            _context.Entry(contenidoEntrega).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!contenidoEntregaExists(id))
+                {
+                    return NotFound("No se encontro el contenido con el id: " + id);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok(updateContenidoEntrega);
+        }
+
+        // POST: api/contenidoEntrega
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost("post")]
+        public async Task<ActionResult<contenidoEntrega>> PostcontenidoEntrega(AddContenidoEntregaDto addContenidoEntrega, int idEntregaPt)
+        {
+            var entregaPt = await _context.contenidoEntrega.FindAsync(idEntregaPt);
+
+            if (entregaPt == null)
+            {
+                return BadRequest("Id de Entrega no valido: " + idEntregaPt);
+            }
+
+            var contenidoEntrega = _mapper.Map<contenidoEntrega>(addContenidoEntrega);
+            contenidoEntrega.idEntregaPt = idEntregaPt;
+
+            _context.contenidoEntrega.Add(contenidoEntrega);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetcontenidoEntrega", new { id = contenidoEntrega.idContenidoEntrega }, contenidoEntrega);
+        }
+
+        private bool contenidoEntregaExists(int id)
+        {
+            return _context.contenidoEntrega.Any(e => e.idContenidoEntrega == id);
+        }
+    }
+}
---
+++ Controllers/ProductoTerminado/detalleEntregaController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sistema_Produccion_3_Backend.DTO.ProductoTerminado;
+using Sistema_Produccion_3_Backend.Models;
+
+namespace Sistema_Produccion_3_Backend.Controllers.ProductoTerminado
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class detalleEntregaController : ControllerBase
+    {
+        private readonly base_nuevaContext _context;
+        private readonly IMapper _mapper;
+
+        public detalleEntregaController(base_nuevaContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        // GET: api/detalleEntrega
+        [HttpGet("get")]
+        public async Task<ActionResult<IEnumerable<DetalleEntregaDto>>> GetdetalleEntrega(int idEntregaPt)
+        {
+            var detalleEntrega = await _context.detalleEntrega
+                .Where(d => d.idEntregaPt == idEntregaPt)
+                .ToArrayAsync();
+
+            if (detalleEntrega == null || detalleEntrega.Length == 0)
+            {
+                return NotFound("No se encontro detalles relacionados con el reporte con id: " + idEntregaPt);
+            }
+
+            var detalleEntregaDto = _mapper.Map<List<DetalleEntregaDto>>(detalleEntrega);
+
+            return Ok(detalleEntregaDto);
+        }
+
+        // GET: api/detalleEntrega/5
+        [HttpGet("get/id/")]
+        public async Task<ActionResult<DetalleEntregaDto>> GetdetalleEntregaId(int id)
+        {
+            var detalleEntrega = await _context.detalleEntrega.FindAsync(id);
+
+            var detalleEntregaDto = _mapper.Map<DetalleEntregaDto>(detalleEntrega);
+
+            if (detalleEntregaDto == null)
+            {
+                return NotFound("No se encuentra el detalle con el id: " + id);
+            }
+
+            return Ok(detalleEntregaDto);
+        }
+
+        // PUT: api/detalleEntrega/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("put/id/")]
+        public async Task<IActionResult> PutdetalleEntrega(int id, UpdateDetalleEntregaDto updateDetalleEntrega)
+        {
+            var detalleEntrega = await _context.detalleEntrega.FindAsync(id);
+
+            if (detalleEntrega == null)
+            {
+                return NotFound("No se encontro el detalle con el id: " + id);
+            }
+
+            _mapper.Map(updateDetalleEntrega, detalleEntrega);
+            _context.Entry(detalleEntrega).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!detalleEntregaExists(id))
+                {
+                    return NotFound("No se encontro el detalle con el id: " + id);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok(updateDetalleEntrega);
+        }
+
+        // POST: api/detalleEntrega
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost("post")]
+        public async Task<ActionResult<detalleEntrega>> PostdetalleEntrega(AddDetalleEntregaDto addDetalleEntrega, int idEntregaPt)
+        {
+            var detallePt = await _context.detalleEntrega.FindAsync(idEntregaPt);
+
+            if (detallePt == null)
+            {
+                return BadRequest("Id de detalle no valido: " + idEntregaPt);
+            }
+
+            var detalleEntrega = _mapper.Map<detalleEntrega>(addDetalleEntrega);
+            detalleEntrega.idEntregaPt = idEntregaPt;
+
+            _context.detalleEntrega.Add(detalleEntrega);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetdetalleEntrega", new { id = detalleEntrega.idDetalleEtrega }, detalleEntrega);
+        }
+
+        private bool detalleEntregaExists(int id)
+        {
+            return _context.detalleEntrega.Any(e => e.idDetalleEtrega == id);
+        }
+    }
+}
