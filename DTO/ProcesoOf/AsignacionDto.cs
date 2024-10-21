994c15130de8f9cafebc90155666da0e8a4a8a0e
Sistemas2 <a.sistemas2@im.com.sv>
2024-10-21 15:20:58 -0600

    Cambios menores

---
+++ DTO/ProcesoOf/AsignacionDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sistema_Produccion_3_Backend.DTO.ProcesoOf
+{
+    public class AsignacionDto
+    {
+        public int idAsignacion { get; set; }
+
+        public string? user { get; set; }
+
+        public int? idProceso { get; set; }
+    }
+}
