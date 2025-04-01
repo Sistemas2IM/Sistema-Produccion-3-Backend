b659823c74f9eb06b13ab71ecff4bda3ad6aa964
Sistemas2 <a.sistemas2@im.com.sv>
2025-04-01 10:16:37 -0600

    Agregados endpoints get para tableros y procesos por idArea

--- Controllers/TablerosOf/TablerosGestionController.cs
+++ Controllers/TablerosOf/TablerosGestionController.cs
@@ -71,6 +71,38 @@ namespace Sistema_Produccion_3_Backend.Controllers.TablerosOf
             return Ok(tablerosDto);
         }
 
+        // GET: api/tableros por area
+        [HttpGet("get/tableros/area{idArea}")]
+        public async Task<ActionResult<IEnumerable<TablerosOfDto>>> GetTablerosArea(int idArea)
+        {
+            var tableros = await _context.tablerosOf
+                .Where(f => f.idArea == idArea)
+                .Include(u => u.idAreaNavigation)
+                .ToListAsync();
+            var tablerosDto = _mapper.Map<List<TablerosOfDto>>(tableros);
+            return Ok(tablerosDto);
+        }
+
+        // GET: api/procesoOf por Area
+        [HttpGet("get/procesosOf/area{idArea}")]
+        public async Task<ActionResult<IEnumerable<ProcesoOfDto>>> GetprocesoOfArea(int idArea)
+        {
+            var procesoOf = await _context.procesoOf
+                .Where(f => f.idTableroNavigation.idArea == idArea)
+                .Include(u => u.detalleOperacionProceso)
+                .ThenInclude(o => o.idOperacionNavigation)
+                .Include(m => m.tarjetaCampo)
+                .Include(s => s.tarjetaEtiqueta)
+                .ThenInclude(e => e.idEtiquetaNavigation)
+                .Include(d => d.idPosturaNavigation)
+                .Include(c => c.idTableroNavigation)
+                .Include(v => v.idMaterialNavigation)
+                .Include(f => f.oFNavigation)
+                .ToListAsync();
+            var procesoOfDto = _mapper.Map<List<ProcesoOfDto>>(procesoOf);
+            return Ok(procesoOfDto);
+        }
+
         // GET: api/tarjetaOf VENDEDORES
         [HttpGet("get/vendedor{vendedor}")]
         public async Task<ActionResult<IEnumerable<TarjetaOfDto>>> GettarjetaOf(string vendedor)
